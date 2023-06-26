71b85e849d7283a976b75e60cce807944eba9c99
PotalaMeghana <135151485+PotalaMeghana@users.noreply.github.com>
2023-06-26 21:46:14 +0530

    Update CategoryReportDAO.java

--- src/eStoreProduct/DAO/admin/CategoryReportDAO.java
+++ src/eStoreProduct/DAO/admin/CategoryReportDAO.java
@@ -3,5 +3,5 @@ import java.util.List;
 import eStoreProduct.model.admin.output.CategoryReportViewModel;
 
 public interface CategoryReportDAO {
-    public List<CategoryReportViewModel> getCatRep();
+    public List<CategoryReportViewModel> getCategoryReport();
 }
