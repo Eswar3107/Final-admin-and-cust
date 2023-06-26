650858b2691a1501857485fb1a18c2c64f66775d
PotalaMeghana <135151485+PotalaMeghana@users.noreply.github.com>
2023-06-26 18:21:06 +0530

    Update ForgotController.java

--- Customer_Side/eStoreProduct/controller/ForgotController.java
+++ Customer_Side/eStoreProduct/controller/ForgotController.java
@@ -1,54 +1,3 @@
-/*
- * package eStoreProduct.controller; import javax.servlet.http.HttpSession;
- * 
- * import org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.stereotype.Controller; import
- * org.springframework.ui.Model; import
- * org.springframework.validation.annotation.Validated; import
- * org.springframework.web.bind.annotation.ModelAttribute; import
- * org.springframework.web.bind.annotation.PostMapping; import
- * org.springframework.web.bind.annotation.RequestMapping; import
- * org.springframework.web.bind.annotation.RequestMethod; import
- * org.springframework.web.bind.annotation.RequestParam; import
- * org.springframework.web.bind.annotation.ResponseBody;
- * 
- * import eStoreProduct.DAO.cartDAO; import eStoreProduct.DAO.customerDAO;
- * import eStoreProduct.model.emailSend;
- * 
- * @Controller public class ForgotController {
- * 
- * String generateotp; String finalemail; customerDAO cdao;
- * 
- * @Autowired public ForgotController(customerDAO customerdao) { cdao =
- * customerdao; //cartdao1 = cartdao; }
- * 
- * @RequestMapping(value = "/forgotPassword", method = RequestMethod.GET) public
- * String forgotPswd(Model model) { System.out.println("mail forget Page");
- * 
- * // // call the view return "forgotPage"; }
- * 
- * @PostMapping("/otpAction")
- * 
- * @ResponseBody public String sendOTP(@RequestParam("email") String email) {
- * finalemail = email; generateotp = (new emailSend()).sendEmail(email);
- * 
- * return generateotp; // Return the generated OTP as the response }
- * 
- * @PostMapping("/validateOTP")
- * 
- * @ResponseBody public String validateOTP(@RequestParam("otp12") String otp) {
- * System.out.print("validate"+otp);
- * 
- * if (otp.equals(generateotp)) { return "valid"; } else { // OTP is invalid
- * return "invalid"; } }
- * 
- * @RequestMapping(value = "/updatepwd") public String
- * usersignup(@RequestParam("psd2") String p2, Model model) {
- * cdao.updatePassword(p2, finalemail);
- * 
- * return "signIn"; } }
- */
-
 package eStoreProduct.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -70,81 +19,57 @@ public class ForgotController {
 	String finalemail;
 	customerDAO cdao;
 	custCredModel cust;
-	java.time.LocalDateTime t1;// = java.time.LocalDateTime.now();
+	java.time.LocalDateTime t1;
+	//dependency injection
 	@Autowired
 	public ForgotController(customerDAO customerdao) {
 		cdao = customerdao;
-		//cartdao1 = cartdao;
 	}
+	//url mapping to open forgot password page
 	@RequestMapping(value = "/forgotPassword", method = RequestMethod.GET)
-	public String forgotPswd(Model model) {
-		//System.out.println("mail forget Page");
-
-		// // call the view
+	public String forgotPassword(Model model) {
+		// call the view
 		return "forgotPage";
 	}
+	// url mapping to check the email is present in database or not
+	@PostMapping("/emailValid")
+	@ResponseBody
+	public String verifyEmail(@RequestParam("email") String email) {
+		 finalemail = email;
+		//call method to get customer with given email
+		 cust = cdao.getCustomerByEmail(email);
+		//return response to the ajax call
+		if(cust==null)
+			return "no";
+		return "yes"; 
+	}
+	//url mapping to send otp to the user mail
 	@PostMapping("/otpAction")
 	@ResponseBody
 	public String sendOTP(@RequestParam("email") String email) {
 		 finalemail = email;
-		 cust = cdao.getCustomerByEmail(email);
-		
+		//send mail by generating the otp
 		 generateotp = (new emailSend()).sendEmail(email);
+		//saving the time of otp generated
 		 t1 = java.time.LocalDateTime.now();
-		// System.out.println("pevious time "+t1);
-		// cust=cdao.updatelastlogin(cust.getCustId());
-		 //System.out.println("updated time "+cust.getCustLastLoginDate());
-
-		return generateotp; // Return the generated OTP as the response
+		// Return the generated OTP as the response
+		return generateotp; 
 	}
-	
-	@PostMapping("/emailValid")
-	@ResponseBody
-	public String verifyEmail(@RequestParam("email") String email) {
-		//System.out.println("email valid method");
-		 finalemail = email;
-		 cust = cdao.getCustomerByEmail(email);
-		if(cust==null)
-			return "no";
-		return "yes"; // Return the generated OTP as the response
-	}
-
-	/*
-	 * @PostMapping("/validateOTP")
-	 * 
-	 * @ResponseBody public String validateOTP(@RequestParam("otp12") String otp) {
-	 * System.out.println("validate"+otp);
-	 * 
-	 * if (otp.equals(generateotp)) { java.sql.Timestamp timestamp1 =
-	 * cust.getCustLastLoginDate(); java.time.LocalDateTime t1 =
-	 * timestamp1.toLocalDateTime(); java.time.LocalDateTime t2 =
-	 * java.time.LocalDateTime.now(); java.time.Duration duration =
-	 * java.time.Duration.between(t1, t2); System.out.println("t1:"+t1);
-	 * System.out.println("t2:"+t2); long hours = duration.toHours(); long minutes =
-	 * duration.toMinutes(); long seconds = duration.getSeconds();
-	 * System.out.println("seconds:"+duration.getSeconds());
-	 * if(duration.getSeconds()<=30) { return "valid"; } else return "no"; } else {
-	 * // OTP is invalid return "invalid"; } }
-	 * 
-	 */
+	//url mapping to validate the otp given by the customer
 	@PostMapping("/validateOTP")
 	@ResponseBody
 	public String validateOTP(@RequestParam("otp12") String otp) {
-	   // System.out.println("validate" + otp);
-
-	    if (otp.equals(generateotp)) {
-	        //java.sql.Timestamp timestamp1 = cust.getCustLastLoginDate();
-	        //java.time.LocalDateTime t1 = timestamp1.toLocalDateTime();
+	  if (otp.equals(generateotp)) {
+		  //getting the time of otp submitted
 	        java.time.LocalDateTime t2 = java.time.LocalDateTime.now();
+		  //find the difference and validate the otp
 	        java.time.Duration duration = java.time.Duration.between(t1, t2);
-	       // System.out.println("t1: " + t1);
-	        ///System.out.println("t2: " + t2);
-	       // System.out.println();
-	        long seconds = duration.getSeconds();
-	       // System.out.println("seconds: " + seconds);
+	       long seconds = duration.getSeconds();
 	        if (seconds <= 30) {
+			//OTP is valid and submitted within time
 	            return "valid";
 	        } else {
+			//OTP is valid but not submitted in time
 	            return "no";
 	        }
 	    } else {
@@ -152,9 +77,10 @@ public class ForgotController {
 	        return "invalid";
 	    }
 	}
-
+        //url mapping for saving new user password
 	@RequestMapping(value = "/updatepwd")
-	public String usersignup(@RequestParam("psd2") String p2, Model model) {
+	public String updateUserNewPassword(@RequestParam("psd2") String p2, Model model) {
+		//updating user password in database
 		cdao.updatePassword(p2, finalemail);
 
 		return "signIn";
