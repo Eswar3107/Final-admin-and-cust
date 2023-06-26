028a07680fdb868ae133bb85ecfff33633380d0a
purpletv <81405384+purpletv@users.noreply.github.com>
2023-06-26 18:18:05 +0530

    Update RegionDAO.java

--- src/eStoreProduct/DAO/admin/RegionDAO.java
+++ src/eStoreProduct/DAO/admin/RegionDAO.java
@@ -7,8 +7,9 @@ import eStoreProduct.model.admin.output.RegionsOutput;
 
 
 public interface RegionDAO {
+	// to get all regions
 	List<RegionsOutput> getRegions();
-
+	
 	boolean addRegion(Regions reg);
 
 	void removeRegion(int id);
