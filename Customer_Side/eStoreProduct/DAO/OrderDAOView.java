0c819b1b605d2443ad1c944b340a0e9acac5e47b
Eswar3107 <85944281+Eswar3107@users.noreply.github.com>
2023-06-26 20:27:38 +0530

    Update OrderDAOView.java

--- Customer_Side/eStoreProduct/DAO/OrderDAOView.java
+++ Customer_Side/eStoreProduct/DAO/OrderDAOView.java
@@ -1,4 +1,3 @@
-
 package eStoreProduct.DAO;
 
 import java.util.List;
@@ -6,21 +5,29 @@ import java.util.List;
 import eStoreProduct.model.OrdersViewModel;
 
 public interface OrderDAOView {
-	public List<OrdersViewModel> getorderProds(int c);
+  
+  // Retrieve ordered products for a given customer ID
+  public List<OrdersViewModel> getorderProds(int c);
 
-	public OrdersViewModel OrdProductById(int c, Integer productId);
+  // Retrieve the details of a specific ordered product by customer ID and product ID
+  public OrdersViewModel OrdProductById(int c, Integer productId);
 
-	public void cancelorderbyId(Integer productId, int orderId);
+  // Cancel an order in orderProducts table by product ID and order ID
+  public void cancelorderbyId(Integer productId, int orderId);
 
-	public String getShipmentStatus(int productId, int orderId);
+  // Get the shipment status of an order by product ID and order ID
+  public String getShipmentStatus(int productId, int orderId);
 
-	public List<OrdersViewModel> sortProductsByPrice(List<OrdersViewModel> orderLists, String sortOrder);
+  // Sort the list of ordered products by price based on the given sort order
+  public List<OrdersViewModel> sortProductsByPrice(List<OrdersViewModel> orderLists, String sortOrder);
 
-	public boolean areAllProductsCancelled(int orderId);
+  // Check if all products in an order are cancelled
+  public boolean areAllProductsCancelled(int orderId);
 
-	public void updateOrderShipmentStatus(int orderId, String shipmentStatus);
+  // Update the shipment status of an order in Orders table
+  public void updateOrderShipmentStatus(int orderId, String shipmentStatus);
 
-	public List<OrdersViewModel> filterProductsByPriceRange(List<OrdersViewModel> productList, double minPrice,
-			double maxPrice);
-	// public List<OrdersViewModel> getsortbypricel_low_to_high();
+  // Filter the list of ordered products by price range
+  public List<OrdersViewModel> filterProductsByPriceRange(List<OrdersViewModel> productList, double minPrice,
+      double maxPrice);
 }
