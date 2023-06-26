2bc4ddb2fb46398179e300319413d8dae9932a25
Eswar3107 <85944281+Eswar3107@users.noreply.github.com>
2023-06-26 18:31:21 +0530

    Update CustomerController.java

--- Customer_Side/eStoreProduct/controller/CustomerController.java
+++ Customer_Side/eStoreProduct/controller/CustomerController.java
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import eStoreProduct.BLL.FairandGStBLL;
-import eStoreProduct.BLL.OrderIdCreationBLL;
+import eStoreProduct.BLL.BLL;
+import eStoreProduct.BLL.BLLClass2;
 import eStoreProduct.DAO.OrderDAO;
 import eStoreProduct.DAO.ProductDAO;
 import eStoreProduct.DAO.StockUpdaterDAO;
@@ -35,9 +35,8 @@ import eStoreProduct.utility.ProductStockPrice;
 public class CustomerController {
 	customerDAO cdao;
 	cartDAO cartimp;
-
-	FairandGStBLL objbl1;
-	OrderIdCreationBLL bl2;
+	BLL BLL;
+	BLLClass2 bl2;
 	String buytype = null;
 	ProductDAO pdaoimp;
 	OrderDAO odao;
@@ -49,53 +48,58 @@ public class CustomerController {
 
 	@Autowired
 	public CustomerController(cartDAO cartdao, customerDAO customerdao, StockUpdaterDAO stckdao, orderModel om,
-			OrderIdCreationBLL bl2, FairandGStBLL bl1, ProductDAO productdao, OrderDAO odao, walletDAO w) {
+			BLLClass2 bl2, BLL bl1, ProductDAO productdao, OrderDAO odao, walletDAO w) {
 		cdao = customerdao;
 		cartimp = cartdao;
 		this.bl2 = bl2;
-		this.objbl1 = bl1;
+		this.BLL = bl1;
 		pdaoimp = productdao;
 		this.odao = odao;
 		wdao = w;
 		this.om = om;
 		this.stckdao = stckdao;
 	}
-
+        //called when user clicks on profile
 	@RequestMapping(value = "/profilePage")
 	public String sendProfilePage(Model model, HttpSession session) {
+		// Retrieve customer information from the session
 		custCredModel cust = (custCredModel) session.getAttribute("customer");
-		// System.out.println(cust.getCustId());
+		
+		// Add customer information to the model
 		model.addAttribute("cust", cust);
 		return "profile";
 	}
-
-	// on clicking update Profile in profile page
+        //called when user tries to update the profile data
 	@RequestMapping(value = "/updateProfile", method = RequestMethod.POST)
 	public String userupdate(@ModelAttribute("Customer") custCredModel cust, Model model, HttpSession session) {
+		// Update the customer information in the database
 		cdao.updatecustomer(cust);
+		
+		// Retrieve the updated customer information
 		custCredModel custt = cdao.getCustomerById(cust.getCustId());
 		if (custt != null) {
+			// Add the updated customer information to the model
 			model.addAttribute("cust", custt);
 		}
 		return "profile";
 	}
-
-	
-
+        //for payment fair in the cart
 	@GetMapping("/buycartitems")
 	public String confirmbuycart(Model model, HttpSession session) {
-
+		// Retrieve customer information from the session
 		custCredModel cust1 = (custCredModel) session.getAttribute("customer");
 		if (cust1 != null) {
-			System.out.println("Entered into buycartitems");
+			// Calculate the total fair for cart items
 			BLL.calculateTotalfair(cust1);
-			System.out.println("Entered into buycartitems1");
-
-			products = objbl1.GetQtyItems2();
-			System.out.println("Entered into buycartitems2          " + ProductStockPrice.getTotal());
+			
+			// Get the quantity and price information of the cart items
+			products = BLL.GetQtyItems2();
+			
+			// Add the products and buy type information to the model
 			model.addAttribute("products", products);
 			buytype = "cartproducts";
 
+			// Get the wallet amount of the customer
 			wallet Wallet = wdao.getWalletAmount(cust1.getCustId());
 			model.addAttribute("Wallet", Wallet);
 
@@ -103,49 +107,54 @@ public class CustomerController {
 		} else {
 			return "signIn";
 		}
-
 	}
 
 	@GetMapping("/getOrderId")
 	@ResponseBody
 	public String getOrderId(@RequestParam(value = "amt") double amt) {
-		double amountInPaisa = amt; // Convert amount to paisa
+		
+		double amountInPaisa = amt;
+		
+		// Create a Razorpay order and retrieve the order ID
 		String orderId = bl2.createRazorpayOrder(amountInPaisa);
 		return orderId;
 	}
 
+	@GetMapping("/done")
+	public String orderCreation(Model model) {
+		return "redirect:/OrderCreation";
+	}
+        //adding customer shipment address
 	@PostMapping("/confirmShipmentAddress")
 	@ResponseBody
 	public String confirm(@RequestParam(value = "mobile") String mobile,
 			@RequestParam(value = "custsaddress") String custsaddress,
 			@RequestParam(value = "spincode") String spincode, Model model, HttpSession session) {
-		// System.out.println("confirm shipmentaddress");
 		custCredModel cust1 = (custCredModel) session.getAttribute("customer");
 		custCredModel cust2 = new custCredModel();
 		cust2.setCustMobile(mobile);
 		cust2.setCustSAddress(custsaddress);
 		cust2.setCustPincode(spincode);
 		session.setAttribute("cust2", cust2);
-		// cdao.updateshipmentaddress(cust1.getCustId(), shaddr);
 		return "OK";
-
 	}
-
-	
-
+       
 	@PostMapping("/updateshipment")
 	@ResponseBody
 	public String handleFormSubmission(@RequestParam(value = "name") String name,
 			@RequestParam(value = "custSAddress") String caddress, @RequestParam(value = "custSpincode") String pincode,
 			HttpSession session) {
 		custCredModel cust = (custCredModel) session.getAttribute("customer");
+		//checks whether the pincode is valid or not
 		boolean isValid = pdaoimp.isPincodeValid(Integer.parseInt(pincode));
-
+                // if valid case
 		if (isValid) {
 			cust.setCustName(name);
 			cust.setCustSAddress(caddress);
 			cust.setCustSpincode(pincode);
+			//shipment details get updated
 			String update_status = cdao.updateShpimentDetails(cust);
+			//if the shipment details updated
 			if (update_status.equals("Updated"))
 				return "Valid";
 			else
@@ -153,59 +162,63 @@ public class CustomerController {
 		}
 		return "Not Valid";
 	}
-
+        //method for showing invoice 
 	@PostMapping(value = "/invoice")
 	public String invoice(@RequestParam("paymentReference") String id, @RequestParam("total") String total, Model model,
 			HttpSession session, @Validated orderModel om) {
 		custCredModel cust1 = (custCredModel) session.getAttribute("customer");
+		//gets wallet amount of user
 		wallet Wallet = wdao.getWalletAmount(cust1.getCustId());
 		double payamount = Double.parseDouble(total);
+		//total payable amount
 		double totalamount = ProductStockPrice.getTotal();
+		//amount used from wallet
 		double walletusedamount = totalamount - payamount;
 		if (walletusedamount > 0) {
 			double x = Wallet.getAmount() - walletusedamount;
+			//updating remaining wallet amount
 			wdao.updatewallet(x, cust1.getCustId());
 		}
 
 		if (buytype.equals("cartproducts")) {
-			products = objbl1.GetQtyItems2();
+			products = BLL.GetQtyItems2();
 		} else {
 			products = product2;
 		}
+		//getting orders gst
 		om.setGst(BLL.getOrderGST(products));
 		model.addAttribute("payment_id", om.getPaymentReference());
-		System.out.println("order shipping pincode is: " + om.getShippingPincode());
 		odao.insertIntoOrders(om, products);
 		for (ProductStockPrice p : products) {
 			stckdao.updateStocks(p.getProd_id(), p.getQuantity());
-			System.out.println("stock updated");
 		}
-		System.out.println(id);
+		//adding requried attributes to the model
 		model.addAttribute("customer", cust1);
 		model.addAttribute("payid", id);
 		model.addAttribute("products", products);
 		return "invoice";
 	}
-
+         //buying an individual product directly without cart
 	@GetMapping("/buythisproduct")
 	public String buythisproduct(@RequestParam(value = "productId", required = true) int productId,
 			@RequestParam(value = "qty", required = true) int qty, Model model, HttpSession session)
 			throws NumberFormatException, SQLException {
 		product2.clear();
 		custCredModel cust1 = (custCredModel) session.getAttribute("customer");
-		ProductStockPrice product = objbl1.individualTotalfair(cust1, productId, qty);
+		//calculating the fare
+		ProductStockPrice product = BLL.individualTotalfair(cust1, productId, qty);
 		product2.add(product);
 
 		buytype = "individual";
+		//adding wallet amount
 		wallet Wallet = wdao.getWalletAmount(cust1.getCustId());
 		model.addAttribute("Wallet", Wallet);
 
 		model.addAttribute("products", product2);
 
 		return "paymentpreview";
-
 	}
-
+        //verify whether person logged in or not before proceding to buy
 	@GetMapping("/checkloginornot")
 	@ResponseBody
 	public String buyproduct(Model model, HttpSession session) throws NumberFormatException, SQLException {
@@ -215,7 +228,5 @@ public class CustomerController {
 		} else {
 			return "false";
 		}
-
 	}
-
 }
