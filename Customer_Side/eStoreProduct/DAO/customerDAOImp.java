0b5de62ef84f572f146fe5c492e6c96df6ef1b83
PotalaMeghana <135151485+PotalaMeghana@users.noreply.github.com>
2023-06-26 20:09:05 +0530

    Update customerDAOImp.java

--- Customer_Side/eStoreProduct/DAO/customerDAOImp.java
+++ Customer_Side/eStoreProduct/DAO/customerDAOImp.java
@@ -19,7 +19,7 @@ public class customerDAOImp implements customerDAO {
 
 	private final String SQL_INSERT_CUSTOMER = "insert into slam_customers(cust_name,  cust_mobile , cust_regdate ,cust_location , cust_email ,cust_address, cust_pincode , cust_saddress,cust_spincode,cust_status ,cust_lastlogindate, cust_password ) values(?,?,?,?,?,?,?,?,?,?,?,?)";
 	private final String SQL_CHECK_CUSTOMER = "select * from slam_customers where cust_email=? and cust_password=? ";
-
+        //dependency injection
 	@Autowired
 	public customerDAOImp(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
@@ -31,7 +31,7 @@ public class customerDAOImp implements customerDAO {
 	public void persist(custCredModel ccm) {
 		em.persist(ccm);
 	}
-
+	//method to save new customer
 	public boolean createCustomer(custCredModel ccm) {
 		System.out.println("spincode:" + ccm.getCustSpincode());
 		return jdbcTemplate.update(SQL_INSERT_CUSTOMER, ccm.getCustName(), ccm.getCustMobile(), ccm.getCustRegDate(),
@@ -40,18 +40,15 @@ public class customerDAOImp implements customerDAO {
 				ccm.getCustPassword()) > 0;
 	}
 
-	// public custCredModel checkCustomer(String email, String pswd) {
-	// // TODO Auto-generated method stub
-	//
-	// return jdbcTemplate.queryForObject(SQL_CHECK_CUSTOMER, new Object[] { email, pswd }, new customerMapper());
-	// }
 
-	public void updatelastlogin(int cid) {
+	//method to update the last login of the customer
+	public void updateLastLogin(int cid) {
 		String updateQuery = "UPDATE slam_customers SET cust_lastlogindate = CURRENT_TIMESTAMP WHERE cust_id = ?";
 		jdbcTemplate.update(updateQuery, cid);
 	}
 
-	public void updateccm(custCredModel ccm) {
+	//method to update the details updated by the customer
+	public void updateCustomer(custCredModel ccm) {
 		String updateQuery = "UPDATE slam_customers SET cust_name = ?, cust_mobile = ?, cust_location = ?, cust_address = ?,cust_pincode = ?, cust_saddress = ?,cust_spincode=?  WHERE cust_id = ?";
 
 		jdbcTemplate.update(updateQuery, ccm.getCustName(), ccm.getCustMobile(), ccm.getCustLocation(),
@@ -59,52 +56,36 @@ public class customerDAOImp implements customerDAO {
 				ccm.getCustId());
 	}
 
+	//method to get the customer based on id
 	public custCredModel getCustomerById(int id) {
 		String custSelect = "SELECT * FROM slam_customers WHERE cust_id=?";
+		custCredModel cu=null;
 
 		try {
-			custCredModel cu = jdbcTemplate.queryForObject(custSelect, new Object[] { id }, new customerMapper());
+			 cu= jdbcTemplate.queryForObject(custSelect, new Object[] { id }, new customerMapper());
 			return cu;
 		} catch (Exception e) {
-			// Handle the exception appropriately (e.g., logging, throwing custom exception, etc.)
 			e.printStackTrace();
-			return null; // or throw an exception if required
+			return null; 
 		}
 	}
 
-	/*
-	 * public custCredModel getCustomer(String email, String password) { password =
-	 * passwordHashing.hashString(password); System.out.println(email + "" + password); // password =
-	 * passwordHashing.hashString(password);
-	 * 
-	 * System.out.println(email + "" + password); String custSelectQuery =
-	 * "SELECT * FROM slam_customer WHERE cust_email = ? AND cust_password = ?";
-	 * 
-	 * try { custCredModel cu = jdbcTemplate.queryForObject(custSelectQuery, new Object[] { email, password }, new
-	 * customerMapper()); return cu; } catch (Exception e) { // Handle the exception appropriately (e.g., logging,
-	 * throwing custom exception, etc.) e.printStackTrace(); return null; // or throw an exception if required } }
-	 */
-
+	//method to check the customer with usermail and password for validation
 	public custCredModel getCustomer(String email, String password) {
 		password = passwordHashing.hashString(password);
-		// System.out.println(email + " " + password);
-		// password = passwordHashing.hashString(password);
-
-		// System.out.println(email + "" + password);
 		String custSelectQuery = "SELECT * FROM slam_customers WHERE cust_email = ? AND cust_password = ?";
-
+		custCredModel cu=null;
 		try {
-			custCredModel cu = jdbcTemplate.queryForObject(custSelectQuery, new Object[] { email, password },
+			cu = jdbcTemplate.queryForObject(custSelectQuery, new Object[] { email, password },
 					new customerMapper());
-			// System.out.println("query before object "+cu);
 			return cu;
 		} catch (Exception e) {
-			// Handle the exception appropriately (e.g., logging, throwing custom exception, etc.)
 			e.printStackTrace();
-			return null; // or throw an exception if required
+			return null;
 		}
 	}
 
+	//method to update the customer details 
 	public void updatecustomer(custCredModel customer) {
 		String updateQuery = "UPDATE slam_customers SET cust_name = ?, cust_mobile = ?, cust_location = ?, cust_address = ?,cust_pincode = ?, cust_saddress = ?,cust_spincode = ?  WHERE cust_id = ?";
 
@@ -113,28 +94,31 @@ public class customerDAOImp implements customerDAO {
 				customer.getCustSpincode(), customer.getCustId());
 	}
 
+	//method to update the user password
 	public void updatePassword(String password, String email) {
+		//performing hashing before saving in the database
 		password = passwordHashing.hashString(password);
-		// password = passwordHashing.hashString(password);
 		String sql = "UPDATE slam_customers SET cust_password=? WHERE cust_email=?";
 
 		jdbcTemplate.update(sql, password, email);
 	}
 
+	//method to check the email present in the database for signin and signup 
 	@Override
 	public custCredModel getCustomerByEmail(String email) {
 		String custSelect = "SELECT * FROM slam_customers WHERE cust_email=?";
+		custCredModel cu=null;
 
 		try {
-			custCredModel cu = jdbcTemplate.queryForObject(custSelect, new Object[] { email }, new customerMapper());
+			cu = jdbcTemplate.queryForObject(custSelect, new Object[] { email }, new customerMapper());
 			return cu;
 		} catch (Exception e) {
-			// Handle the exception appropriately (e.g., logging, throwing custom exception, etc.)
-			// e.printStackTrace();
-			return null; // or throw an exception if required
+			 e.printStackTrace();
+			return null;
 		}
 	}
 
+	//method to update the shipment details when placing order
 	@Override
 	public String updateShpimentDetails(custCredModel ccm) {
 		String update_shipment_details = "update slam_customers set cust_name=? , cust_saddress=? , cust_spincode=? where cust_id=?";
