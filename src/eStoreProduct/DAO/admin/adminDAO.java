bf097693c2c93b60b359406db2d23cc4b495f380
Eswar3107 <85944281+Eswar3107@users.noreply.github.com>
2023-06-26 20:15:38 +0530

    Update adminDAO.java

--- src/eStoreProduct/DAO/admin/adminDAO.java
+++ src/eStoreProduct/DAO/admin/adminDAO.java
@@ -3,8 +3,10 @@ package eStoreProduct.DAO.admin;
 import eStoreProduct.model.admin.entities.adminModel;
 
 public interface adminDAO {
+  
+  // Retrieve admin information based on email and password
+  public adminModel getAdmin(String email, String psd);
 
-	public adminModel getAdmin(String email, String psd);
-
-	public void updateAdmin(adminModel am);
+  // Update admin information
+  public void updateAdmin(adminModel am);
 }
