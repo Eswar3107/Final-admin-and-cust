84d531d51324eb6464466e98eb551758a187bea9
PotalaMeghana <135151485+PotalaMeghana@users.noreply.github.com>
2023-06-26 21:26:34 +0530

    Update ProdStockDAOImp.java

--- src/eStoreProduct/DAO/admin/ProdStockDAOImp.java
+++ src/eStoreProduct/DAO/admin/ProdStockDAOImp.java
@@ -23,21 +23,25 @@ public class ProdStockDAOImp implements ProdStockDAO {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
+	//method to get the stock 
 	@Override
 	public List<ProdStock> getAllProdStocks() {
 		return jdbcTemplate.query(PD_STK_QUERY, new ProductStockRowMapper());
 	}
 
+	//get method to stock of product by its id
 	@Override
 	public ProdStock getProdStockById(int prodId) {
 		return jdbcTemplate.queryForObject(SELECT_PRD_STK_QUERY, new ProductStockRowMapper(), prodId);
 	}
 
+	//method to get product price by id
 	@Override
 	public double getProdPriceById(int prodId) {
 		return jdbcTemplate.queryForObject(PD_PRICE_QUERY, Double.class, prodId);
 	}
 
+	//method to get product mrp by id
 	@Override
 	public double getProdMrpById(int prodId) {
 		return jdbcTemplate.queryForObject(PD_MRP_QUERY, Double.class, prodId);
